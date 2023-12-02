925b24765811e454670ebb8b28d61a22dcdb79d3
Andrés  Díaz Vargas <andres.diaz.vargas@gorillalogic.com>
2023-12-02 12:00:46 -0600

    exercise 5 finished

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,10 @@ import { checkGuess } from "../../game-helpers";
 
 import GuessInput from "../GuessInput/GuessInput";
 import GuessList from "../GuessesList/GuessesList";
+import HappyBanner from "../HappyBanner/HappyBanner";
+import SadBanner from "../SadBanner/SadBanner";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
@@ -13,11 +17,16 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [hasWon, setHasWon] = React.useState(false);
 
   function onSubmitGuess(guess) {
     const checkedGuess = checkGuess(guess, answer);
     console.log(checkedGuess);
-
+    const isCorrect = checkedGuess.reduce((acc, letter) => {
+      letter.status === "correct" ? acc++ : null;
+      return acc;
+    }, 0);
+    setHasWon(isCorrect === 5);
     setGuesses([
       ...guesses,
       {
@@ -31,7 +40,13 @@ function Game() {
   return (
     <>
       <GuessList guesses={guesses} />
-      <GuessInput onSubmitGuess={onSubmitGuess} />
+      {!hasWon && guesses.length < NUM_OF_GUESSES_ALLOWED && (
+        <GuessInput onSubmitGuess={onSubmitGuess} />
+      )}
+      {guesses.length >= NUM_OF_GUESSES_ALLOWED && (
+        <SadBanner answer={answer} />
+      )}
+      {hasWon && <HappyBanner numberOfGuesses={guesses.length} />}
     </>
   );
 }
